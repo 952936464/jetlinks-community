6c5ff5e2c76f70c660264a7111db30c79d74d3a5
952936464 <101640475+952936464@users.noreply.github.com>
2023-07-26 10:47:57 +0800

    feat(创建实体类):完成学习计划表中第一步：实体表(商品管理表和订单管理表)设计与实现

---
+++ practice-project/src/main/java/org/jetlinks/community/example/enums/OrderStatus.java
@@ -0,0 +1,32 @@
+package org.jetlinks.community.example.enums;
+
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hswebframework.web.dict.Dict;
+import org.hswebframework.web.dict.EnumDict;
+import org.hswebframework.web.dict.I18nEnumDict;
+import java.*;
+
+
+@Getter
+@AllArgsConstructor
+@Dict("order-type")
+@JsonDeserialize(contentUsing = EnumDict.EnumDictJSONDeserializer.class)
+public enum OrderStatus implements I18nEnumDict<String> {
+    //订单状态：包含3种
+    unpaid("未付款"),
+    paid("付款成功"),
+    successAccept("确认收货")
+    ;
+
+    private final String text;
+    @Override
+    public String getValue() {
+        return name();
+    }
+
+
+
+}
