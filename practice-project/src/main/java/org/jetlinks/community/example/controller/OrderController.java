dbfda3bf0d729da4b2ba43a344ed3536bd586563
952936464 <101640475+952936464@users.noreply.github.com>
2023-07-28 10:54:36 +0800

    feat(产品和订单的crud):商品和订单的crud以及自定义查询

--- practice-project/src/main/java/org/jetlinks/community/example/controller/OrderController.java
+++ practice-project/src/main/java/org/jetlinks/community/example/controller/OrderController.java
@@ -1,12 +1,11 @@
 package org.jetlinks.community.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.hswebframework.ezorm.rdb.mapping.ReactiveRepository;
-import org.hswebframework.ezorm.rdb.mapping.defaults.record.Record;
-import org.hswebframework.utils.file.FileUtils;
 import org.hswebframework.web.api.crud.entity.PagerResult;
 import org.hswebframework.web.api.crud.entity.QueryParamEntity;
 import org.hswebframework.web.authorization.annotation.Resource;
@@ -14,23 +13,22 @@ import org.hswebframework.web.crud.query.QueryHelper;
 import org.hswebframework.web.crud.service.ReactiveCrudService;
 import org.hswebframework.web.crud.web.reactive.ReactiveServiceCrudController;
 import org.jetlinks.community.example.entity.OrderEntity;
-import org.jetlinks.community.example.entity.OrderExtended;
 import org.jetlinks.community.example.entity.ProductEntity;
+import org.jetlinks.community.example.enums.OrderStatus;
 import org.jetlinks.community.example.enums.OrderType;
 
+import org.jetlinks.community.example.enums.ProductType;
 import org.jetlinks.community.example.service.OrderService;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.persistence.criteria.Order;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/order/crud")
 @AllArgsConstructor
 @Resource(id = "example", name = "增删改查演示")//资源定义,用于权限控制
-@Tag(name = "增删改查演示") //swagger
+@Tag(name = "order增删改查演示") //swagger
 public class OrderController implements ReactiveServiceCrudController<OrderEntity, String> {
 
     private final OrderService orderService;
@@ -38,32 +36,65 @@ public class OrderController implements ReactiveServiceCrudController<OrderEntit
 
     private final QueryHelper queryHelper;
 
-    //增：有bug没写 todo
-
-    //删
-    @DeleteMapping("/_practice/{id}")
-    public Mono<Integer> deleteOrderEntityById(@PathVariable String id){
-        return orderRepository.deleteById(id);
+    //增
+    @PostMapping("/_practice/_add")
+    @Operation(summary = "增加订单订单号根据流水号和商品id决定")
+    public Mono<Integer> addOrderEntity(@RequestBody OrderEntity orderEntity) {
+        String seq = UUID.randomUUID().toString().replaceAll("-", "");
+        String productId = orderEntity.getProductId();
+        String s = OrderEntity.generateOrderId(seq, productId);
+        orderEntity.setId(s);
+        orderEntity.setOrderSerialNumber(seq);
+        return orderService.insert(orderEntity);
     }
-    //查
-    @GetMapping("/_practice/{id}")
-    public Mono<OrderEntity> queryOrderEntityById(@PathVariable String id){
-        return orderRepository.findById(id);
-    }
 
 
-    //根据订单状态查询符合的订单详情：包含订单表所有字段，及商品id对应的商品名称及商品分类
-    @GetMapping("/_practice/_detail/_query/{orderStatus}")
-    public Mono<PagerResult<Record>> queryByOrderStatus(@PathVariable String orderStatus) {
+    @GetMapping("/_practice/_detail/_query")
+    @Operation(summary = "根据请求体查询订单所有数据以及商品名称类型")
+    public Mono<PagerResult<OrderInfo>> queryByParamEntity(QueryParamEntity queryParam){
         return queryHelper
-            .select("select o.*, p.id, p.name, p.product_type from s_order as o " +
-            "left join s_product as p on o.product_Id = p.id where o.order_status = ? ", orderStatus)
+            .select(OrderInfo.class)
+            .all(OrderEntity.class)
+            .as(ProductEntity::getProductType, OrderInfo::setProductType)
+            .as(ProductEntity::getName, OrderInfo::setProductName)
+            .from(OrderEntity.class)
+            .leftJoin(ProductEntity.class, spec -> spec.is(OrderEntity::getProductId, ProductEntity::getId))
+            .where(queryParam)
             .fetchPaged();
     }
 
     @Override
     public ReactiveCrudService<OrderEntity, String> getService() {
         return orderService;
+    }
+
+
+    @Getter
+    @Setter
+    public static class OrderInfo{
+        private String id;
+
+        private String productId;
+
+        private String productName;
+
+        private ProductType productType;
+
+        private String orderId;
+
+        private  String orderSerialNumber;
+
+        private OrderType orderType;
+
+        private OrderStatus orderStatus;
+
+        private String creatorId;
+
+        private String modifierId;
+
+        private Long modifyTime;
+
+        private Long createTime;
     }
 
 
--- practice-project/src/main/java/org/jetlinks/community/example/controller/ProductController.java
+++ practice-project/src/main/java/org/jetlinks/community/example/controller/ProductController.java
@@ -1,5 +1,7 @@
 package org.jetlinks.community.example.controller;
 
+import groovyjarjarpicocli.CommandLine;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,49 +11,47 @@ import org.hswebframework.web.api.crud.entity.PagerResult;
 import org.hswebframework.web.authorization.annotation.Resource;
 import org.hswebframework.web.crud.query.QueryHelper;
 import org.hswebframework.web.crud.web.reactive.ReactiveServiceCrudController;
+import org.hswebframework.web.validator.ValidatorUtils;
+import org.jetlinks.community.example.entity.OrderEntity;
 import org.jetlinks.community.example.entity.ProductEntity;
 import org.jetlinks.community.example.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RestController
 @RequestMapping("/product/crud")
 @AllArgsConstructor
 @Getter
 @Resource(id = "example", name = "增删改查演示")//资源定义,用于权限控制
-@Tag(name = "增删改查演示") //swagger
+@Tag(name = "product增删改查演示") //swagger
 public class ProductController implements ReactiveServiceCrudController<ProductEntity, String> {
     private final ProductService service;
 
     private final QueryHelper queryHelper;
     private final ReactiveRepository<ProductEntity, String> productRepository;
 
-    //增
-    @PostMapping("/_practice/_add")
-    public Mono<Void> addProductEntity(@RequestBody Flux<ProductEntity> productEntityFlux) {
-        return productRepository
-            .save(productEntityFlux)
-            .then();
-    }
-    //删
-    @DeleteMapping("/_practice/{id}")
-    public Mono<Integer> deleteProductEntityById(@PathVariable String id){
-        return productRepository.deleteById(id);
-    }
-    //查
-    @GetMapping("/_practice/{id}")
-    public Mono<ProductEntity> queryProductEntityById(@PathVariable String id){
-        return productRepository.findById(id);
-    }
 
-    //根据商品批次查询关联的订单的数量接口
+
     @GetMapping("/_practice/_detail/query/{productBatch}")
+    @Operation(summary = "根据商品批次查询关联的订单的数量接口")
     public Mono<Integer> countByProductBatch(@PathVariable String productBatch) {
         return queryHelper
-            .select("select * from (select o.id, p.product_Batch from s_order as o " +
-            "left join s_product as p on o.product_Id = p.id) as T where T.product_batch = ?"
-            , productBatch)
+            .select(OrderInfo.class)
+            .as(OrderEntity::getId,OrderInfo::setId)
+            .from(OrderEntity.class)
+            .leftJoin(ProductEntity.class, spec -> spec.is(OrderEntity::getProductId, ProductEntity::getId))
+            .where(dsl -> dsl.is(ProductEntity::getProductBatch,productBatch))
             .count();
     }
+
+    @Getter
+    @Setter
+    public static class OrderInfo{
+        private String id;
+    }
+
 }
